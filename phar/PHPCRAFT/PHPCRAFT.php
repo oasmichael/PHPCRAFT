75733996148586f1f25302a1deb167b7daf1a34d
tangqiang <tangqiang@oasgames.com>
2015-04-16 20:49:36 +0800

    +

--- phar/PHPCRAFT/PHPCRAFT.php
+++ phar/PHPCRAFT/PHPCRAFT.php
@@ -12,4 +12,30 @@
  *
  */
 
-echo "\rppppppp h     h  ppppppp ccccccc rrrrrrr    aaa   fffffff ttttttt\r\np     p h     h  p     p c       r     r   a   a  f          t\r\nppppppp hhhhhhh  ppppppp c       rrrrrrr  aaaaaaa fffffff    t\r\np       h     h  p       c       r   r    a     a f          t\r\np       h     h  p       ccccccc r    rr  a     a f          t\r\n";
+//draw project logo
+
+use PHPCRAFT\core\Main;
+
+$main = new Main();
+
+//    Main::drawLogo();
+
+echo "[INFO] PHPCRAFT Version 1.0\r\n";
+echo "[INFO] Server start\r\n";
+
+if (!is_dir("config")) {
+    mkdir("config");
+}
+
+if (!file_exists("./config/server.php")) {
+    copy('phar://PHPCRAFT.phar/config/server.php', './config/server.php');
+}
+
+while (1) {
+    echo "[INFO] Listening on port 127.0.0.1:8000\r\n";
+    sleep(2);
+}
+
+for ($i = 0; $i < 5; $i++) {
+    echo "[INFO]";
+}
---
+++ phar/PHPCRAFT/core/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ *
+ * ppppppp h     h  ppppppp ccccccc rrrrrrr    aaa   fffffff ttttttt
+ * p     p h     h  p     p c       r     r   a   a  f          t
+ * ppppppp hhhhhhh  ppppppp c       rrrrrrr  aaaaaaa fffffff    t
+ * p       h     h  p       c       r   r    a     a f          t
+ * p       h     h  p       ccccccc r    rr  a     a f          t
+ *
+ * @author timo
+ *
+ */
+
+namespace PHPCRAFT\core;
+
+class Main
+{
+    public static function drawLogo()
+    {
+        echo "\r\nppppppp h     h  ppppppp ccccccc rrrrrrr    aaa   fffffff ttttttt\r\np     p h     h  p     p c       r     r   a   a  f          t\r\nppppppp hhhhhhh  ppppppp c       rrrrrrr  aaaaaaa fffffff    t\r\np       h     h  p       c       r   r    a     a f          t\r\np       h     h  p       ccccccc r    rr  a     a f          t\r\n\r\n";
+    }
+}
